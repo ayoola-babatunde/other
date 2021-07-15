7c7bce40510b5eedb4275d3f09fad5d8f22dae6c
ayoola-babatunde <ayoola@minerva.kgi.edu>
2021-07-14 22:45:07 -0700

    Appearances, ordinal numbers

--- wilty.py
+++ wilty.py
@@ -1,15 +1,18 @@
 #%%
 import json
-
-    
+import pickle
+from itertools import chain
+from collections import Counter
+from num2words import num2words as n2w
     
 #%%
+#Convert tables of series to dict
+#{S07x07: Date\tDavid_team\tLee_team\tScore\n}
 try: 
     with open("library.json") as jsonfile: 
         library = json.load(jsonfile) 
 except FileNotFoundError: 
     library = {}
-
     with open("pastseries.txt", 'r') as f: 
         for line in f: 
             try: 
@@ -24,22 +27,51 @@ except FileNotFoundError:
     f.close()
 
 #%%
+#Convert dict to sequential list of all guests
+
+try: 
+    with open("allguests.txt", "rb") as f: 
+        allguests = pickle.load(f)
+except FileNotFoundError: 
+    allguests = []
+    for title in library: 
+        deet = library[title] #deet = details
+        groups = deet.split('\t')[1:3]
+        for group in groups: 
+            guests = group.split(' and ')
+            allguests = chain(allguests, guests)
+    with open("allguests.txt", "wb") as f: 
+        pickle.dump(list(allguests), f)
+
+#%%
+#Manual all guests list reference
+#{series:index of last item}
+reference = {1:23, 2:57, 3:92, 4:125, 5:159, 6:193, 7:227, 
+8:269, 9:307, 10:341, 11:383, 12:423, 13:467, 14:511}
+
+#%%
+#Input
 srinput = int(input("Series: "))
 epinput = int(input("Episode: "))
+overallinput = int(input("Overall: "))
+
+#Get series details in library
 totinput = f'{srinput:02d}x{epinput:02d}'
 
 wiki = f'{totinput}\t{library[totinput]}'
 
+#Process details
 details = wiki.split("	")
 
-series, episode = details[0].split('x')
-series, episode = int(series), int(episode)
+series, episode = srinput, epinput
 
 date = details[1]
 
+#guests
 david1, david2  = details[2].split(' and ')
 lee1, lee2  = details[3].split(' and ')
 
+#score
 d_score, l_score = details[4].split('â€“')
 d_score, l_score = int(d_score), int(l_score)
 
@@ -56,10 +88,15 @@ else:
     lresult = f"Lee's team defeated David's team by {l_score} points to {d_score}"
     p5 = "[[Category:Episodes won by Lee's team]]"
 
+#%%
+#Appearances
+n_allguests = allguests[:reference[series]]
+counts = Counter(n_allguests)
 
+#%%
 p1 = f"""
 {{{{Infobox episode
-|overall = 
+|overall = {overallinput}
 |series = {series}
 |episode = {episode}
 |airdate = {date}
@@ -70,7 +107,7 @@ p1 = f"""
 |result = {result}
 |previous = [[Series {series}, Episode {episode-1}]]
 |next = [[Series {series}, Episode {episode+1}]]}}}}
-The '''''' episode of the '''tenth series''' was first broadcast on {date}. It's the - overall episode of ''[[Would I Lie To You?]]''.
+The '''{n2w(episode, ordinal=True)}''' episode of the '''{n2w(series, ordinal=True)} series''' was first broadcast on {date}. It's the {n2w(overallinput, ordinal=True)} overall episode of ''[[Would I Lie To You?]]''.
 """
 
 p2 = f"""
@@ -83,17 +120,17 @@ p2 = f"""
 |- bgcolor="E1E8EF"
 | rowspan="2" |David's team
 |{david1}
-|1st appearance
+|{n2w(counts[david1], to="ordinal_num")} appearance
 |- bgcolor="E1E8EF"
 |{david2}
-|1st appearance
+|{n2w(counts[david2], to="ordinal_num")} appearance
 |- bgcolor="ffffdd"
 | rowspan="2" |Lee's team
 |{lee1}
-|1st appearance
+|{n2w(counts[lee1], to="ordinal_num")} appearance
 |- bgcolor="ffffdd"
 |{lee2}
-|1st appearance
+|{n2w(counts[lee2], to="ordinal_num")} appearance
 |}}
 """
 
@@ -148,3 +185,5 @@ final = p1 + p2 + p3 + p4 + p5
 from pandas.io.clipboard import copy 
 print(final)
 
+
+# %%
